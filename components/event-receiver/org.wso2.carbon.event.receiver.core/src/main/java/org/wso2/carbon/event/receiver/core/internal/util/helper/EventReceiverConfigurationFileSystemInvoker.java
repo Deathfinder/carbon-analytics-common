2642d764b7f004d48aefeafd4f26c24658d671ed
apashko <dfinder77@gmail.com>
2017-07-17 23:45:13 +0300

    fix bug with wrong tenant id when reload fail receivers

--- components/event-receiver/org.wso2.carbon.event.receiver.core/src/main/java/org/wso2/carbon/event/receiver/core/internal/util/helper/EventReceiverConfigurationFileSystemInvoker.java
+++ components/event-receiver/org.wso2.carbon.event.receiver.core/src/main/java/org/wso2/carbon/event/receiver/core/internal/util/helper/EventReceiverConfigurationFileSystemInvoker.java
@@ -22,6 +22,7 @@ import org.apache.axis2.deployment.repository.util.DeploymentFileData;
 import org.apache.axis2.engine.AxisConfiguration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.core.util.CryptoUtil;
 import org.wso2.carbon.event.input.adapter.core.EventAdapterUtil;
 import org.wso2.carbon.event.receiver.core.EventReceiverDeployer;
@@ -169,8 +170,11 @@ public class EventReceiverConfigurationFileSystemInvoker {
         AxisConfiguration axisConfiguration = EventAdapterUtil.getAxisConfiguration();
         EventReceiverDeployer deployer = EventReceiverConfigurationHelper.getEventReceiverDeployer(axisConfiguration);
         try {
+            PrivilegedCarbonContext.startTenantFlow();
+            PrivilegedCarbonContext.getThreadLocalCarbonContext().setTenantId(eventReceiverConfigurationFile.getTenantId(),true);
             deployer.processUndeployment(filePath);
             deployer.processDeployment(new DeploymentFileData(new File(filePath)));
+            PrivilegedCarbonContext.endTenantFlow();
         } catch (DeploymentException e) {
             throw new EventReceiverConfigurationException(e);
         }
